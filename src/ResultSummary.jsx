5b97a3c6182a1b6b3241eab88a6d7530235992a5
Your Name <you@example.com>
2025-07-10 21:46:27 +0700

    respon

--- src/ResultSummary.jsx
+++ src/ResultSummary.jsx
@@ -33,32 +33,27 @@ const ResultSummary = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center mt-10 mx-auto bg-white shadow-2xl overflow-hidden rounded-3xl w-fit">
-      {/* left column */}
-      <div className="bg-blue-600 flex flex-col items-center justify-center pt-5 rounded-3xl text-white">
-        <h3 className=" opacity-70 text-center text-2xl font-medium">
-          Your Result
-        </h3>
-        <div
-          className="flex flex-col items-center justify-center text-center w-40 h-40 rounded-full bg-gradient-to-b from-blue-800/70 
-            to-blue-600/10 mx-auto mt-10 mb-10"
-        >
-          <p className=" font-bold text-7xl m-4">76</p>
-          <p className=" opacity-70">of 100</p>
+     <div className="flex flex-col md:flex-row items-center justify-center mt-10 mx-auto bg-white shadow-2xl overflow-hidden rounded-3xl w-fit">
+      {/* LEFT SIDE */}
+      <div className="bg-blue-600 flex flex-col items-center justify-center pt-5 rounded-3xl text-white w-full">
+        <h3 className="opacity-70 text-center text-2xl font-medium">Your Result</h3>
+        <div className="flex flex-col items-center justify-center text-center w-40 h-40 rounded-full bg-gradient-to-b from-blue-800/70 to-blue-600/10 mx-auto my-10">
+          <p className="font-bold text-7xl m-4">76</p>
+          <p className="opacity-70">of 100</p>
         </div>
         <div className="flex flex-col text-center mb-5">
-          <span className=" font-bold  text-xl">Great</span>
-          <span className=" opacity-70 px-6 mt-5">
+          <span className="font-bold text-xl">Great</span>
+          <span className="opacity-70 px-6 mt-5">
             You scored higher than 65% of <br />
             the people who have taken <br /> these tests.
           </span>
         </div>
       </div>
 
-      {/* right column */}
-      <div className=" p-5">
-        <h3 className=" font-bold text-2xl mb-8">Summary</h3>
-        <div className="">
+      {/* RIGHT SIDE */}
+      <div className="p-5 md:w-80">
+        <h3 className="font-bold text-2xl mb-8">Summary</h3>
+        <div>
           {summary.map((item) => (
             <div
               key={item.title}
@@ -75,7 +70,7 @@ const ResultSummary = () => {
             </div>
           ))}
         </div>
-        <button className=" rounded-3xl bg-blue-950 text-white w-full p-3 font-semibold mt-5">
+        <button className="rounded-3xl bg-blue-950 text-white w-full p-3 font-semibold mt-5">
           Continue
         </button>
       </div>
